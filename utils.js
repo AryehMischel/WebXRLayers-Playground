d913ed64ac96e58da8d5dc70ec46b7b2793c0f85
Aryeh Mischel <aryehmischel@gmail.com>
2024-10-21 15:25:09 -0400

    updated textures

---
+++ utils.js
@@ -0,0 +1,8 @@
+export function onWindowResize(camera, renderer) {
+    console.log('resize')
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
